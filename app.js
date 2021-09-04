dfc3d874bb7e6f34e3565bce60c8af6284288768
Mark Jesper Pillerva <mark123jesper@yahoo.com>
2021-09-04 17:13:50 +0800

    fixed npm script

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ dotenv.config({
 const database = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
-    port: process.env.DATABASE_PORT,
+    port: process.env.DATABASE_PORT || 3400,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE
 });
